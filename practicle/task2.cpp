9fcf9a5e1b8db03eb32156b830cfe4e2adc71add
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-09-29 10:14:09 +0530

    Create task2.cpp

---
+++ practicle/task2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+namespace first{
+int log(int a,int b){
+	return a+b;
+}
+}
+namespace second{
+	float add(float a,float b){
+		return a+b;
+	}
+}
+
+using namespace first;
+using namespace second;
+int main(){
+	std::cout<<add(1,5)<<"\n";
+	std::cout<<add(1,5.9)<<"\n";
+	std::cout<<add(1.6,5)<<"\n";
+	std::cout<<add(0,0)<<"\n";
+	return 0;
+}
