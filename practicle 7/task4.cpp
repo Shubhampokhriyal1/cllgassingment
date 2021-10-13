60b1197be8df62191bd24ce142d5a77ca30ce95d
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-10-13 10:27:26 +0530

    Create task4.cpp

---
+++ practicle 7/task4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int arrNew[3][3];
+    for(int i=0;i<3;i++){
+        
+            for(int j=0;j<3;j++){
+                arrNew[i][j]=10-arr[i][j];
+            }
+            
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arrNew[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
