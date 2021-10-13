c6c673ceb2afb33ba02ce4210f3a6a7a2155e4e6
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-10-13 10:02:36 +0530

    Create task4.cpp

---
+++ practicle 7/task4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    for(int i=0;i<=3/2;i++){
+        if(i!=2-i)
+            for(int j=0;j<3;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[2-i][2-j];
+                arr[2-i][2-j]=temp;
+            }
+         else {
+            for(int j=0;j<3/2;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[2-i][2-j];
+                arr[2-i][2-j]=temp;
+            }
+         }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
