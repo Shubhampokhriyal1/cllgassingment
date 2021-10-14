4a1ab50ce85ec0e8ec8597e3ccfb0016db5c61c9
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-10-14 10:43:59 +0530

    Create task7.5.cpp

---
+++ practicle 7/task7.5.cpp
@@ -0,0 +1,28 @@
+/*printing pattern
+*
+* *
+* * *
+* * * *
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	char arr[n][n];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(j<=i)
+				arr[i][j]='*';
+			else
+				arr[i][j]=' ';
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
