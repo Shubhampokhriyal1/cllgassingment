3833ae2b2a82125b753bae563947e2fbb5248117
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-10-18 10:50:44 +0530

    Delete practicle 8 directory

--- practicle 8/task8.1.cpp
+++
@@ -1,9 +0,0 @@
-/* in this we will check that array pointer can be changed or not*/
-
-#include<iostream>
-using namespace std;
-int main(){
-	int  arr[]={10,4,5,6,7};
-	arr++;
-	return 0;
-}
--- practicle 8/task8.1_v1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int  arr[]={10,4,5,6,7};
-	int *p=arr;
-	cout<<*(p++)<<endl;
-	cout<<*p<<endl;
-	return 0;
-}
