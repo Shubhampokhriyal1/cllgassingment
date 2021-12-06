6d6f57d80c5ade5951eb0917063539969f6c001e
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-12-06 10:23:42 +0530

    Update task2_a.cpp

--- practrical 11/task2_a.cpp
+++ practrical 11/task2_a.cpp
@@ -11,13 +11,15 @@ class A{
   class B:public A{
     public:
     void display(){
+     x=10;
+     y=20
       cout<<"x=>"<<x<<" y=>"<<y;
     }
   };
   int main(){
     B obj;
     obj.x=30;
-    //obj.y=40; protected
+    //obj.y=40;
     obj.display();
     return 0;
   }
