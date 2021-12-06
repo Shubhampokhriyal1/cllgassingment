91fa7c2dbac3fbfc60e1f93c1257d83c4d6d17c5
Shubhampokhriyal1 <82198611+Shubhampokhriyal1@users.noreply.github.com>
2021-12-06 10:20:25 +0530

    Create task2_a.cpp

---
+++ practrical 11/task2_a.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class A{
+ public:
+  int x;
+ protected:
+  int y;
+ private:
+  int x;
+};
+  class B:public A{
+    public:
+    void display(){
+      cout<<"x=>"<<x<<" y=>"<<y;
+    }
+  };
+  int main(){
+    B obj;
+    obj.x=30;
+    //obj.y=40; protected
+    obj.display();
+    return 0;
+  }
